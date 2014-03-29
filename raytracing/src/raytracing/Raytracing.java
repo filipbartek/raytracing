16ed1f545bfa0701e44ca57971a4d55c8b1b5837
Filip Bártek <filip.bartek@hotmail.com>
2014-03-29 16:53:54 +0100

    Raytracing.java: Changed name, removed license placeholder

--- raytracing/src/raytracing/Raytracing.java
+++ raytracing/src/raytracing/Raytracing.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package raytracing;
 
 /**
  *
- * @author TTT
+ * @author Filip Bartek
  */
 public class Raytracing {
 
