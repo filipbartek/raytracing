4844375c0d500d78a43c7c3711933437d8675250
Filip Bártek <filip.bartek@hotmail.com>
2014-03-29 19:43:47 +0100

    Body: Extended `f` description; fixed a typo

--- raytracing/src/main/java/raytracing/Body.java
+++ raytracing/src/main/java/raytracing/Body.java
@@ -7,10 +7,12 @@ import javax.vecmath.Point3f;
  * @author Filip Bartek
  */
 public interface Body {
-    // Implicit function that defines the body
+    // Implicit function that defines the body.
+    // f == 0 iff point is on body surface
+    // f > 0 iff point is outside of body
     float f(Point3f point);
     
-    // Partial derivations of f
+    // Partial derivatives of f
     float fx(Point3f point);
     float fy(Point3f point);
     float fz(Point3f point);
