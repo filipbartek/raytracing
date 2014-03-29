6d0847f0938b8a052fa1e0e83badfba37d9375b1
Filip Bártek <filip.bartek@hotmail.com>
2014-03-29 23:13:22 +0100

    Removed dependency on javax from SphereBody

--- raytracing/src/main/java/raytracing/SphereBody.java
+++ raytracing/src/main/java/raytracing/SphereBody.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package raytracing;
-
-import javax.vecmath.Point3f;
 
 // Represents a sphere with radius `radius` and center in [0, 0, 0].
 public class SphereBody implements Body {
@@ -19,25 +11,25 @@ public class SphereBody implements Body {
 
     @Override
     public float f(float[/*3*/] p) {
-        Point3f point = new Point3f(p);
+        Tuple3f point = new Tuple3f(p);
         return point.x * point.x + point.y * point.y + point.z * point.z - radiusSquare;
     }
 
     @Override
     public float fx(float[/*3*/] p) {
-        Point3f point = new Point3f(p);
+        Tuple3f point = new Tuple3f(p);
         return 2 * point.x;
     }
 
     @Override
     public float fy(float[/*3*/] p) {
-        Point3f point = new Point3f(p);
+        Tuple3f point = new Tuple3f(p);
         return 2 * point.y;
     }
 
     @Override
     public float fz(float[/*3*/] p) {
-        Point3f point = new Point3f(p);
+        Tuple3f point = new Tuple3f(p);
         return 2 * point.z;
     }
     
