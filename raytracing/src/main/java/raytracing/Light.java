2eb8b010fec61a6e796774d000d09389b819f0ac
Filip Bártek <filip.bartek@hotmail.com>
2014-03-30 16:32:06 +0200

    Light: Exposed float[] instead of Tuple3f; added comments

--- raytracing/src/main/java/raytracing/Light.java
+++ raytracing/src/main/java/raytracing/Light.java
@@ -7,6 +7,15 @@ import java.awt.Color;
  * @author Filip Bartek
  */
 public interface Light {
-    public Tuple3f getDirection(Tuple3f point);
+    /**
+     * Gets the direction of the light in a given point.
+     * @param point
+     * @return Directional vector of light in `point`
+     */
+    public float[/*3*/] getDirection(float[/*3*/] point);
+    
+    /**
+     * @return Color of the light
+     */
     public Color getColor();
 }
