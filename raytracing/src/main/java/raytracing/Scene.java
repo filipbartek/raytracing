989616833d2fe9dea3ac049a4a16d343e9109d5b
Filip Bártek <filip.bartek@hotmail.com>
2014-03-30 17:18:41 +0200

    Scene: Moved example usage under class

--- raytracing/src/main/java/raytracing/Scene.java
+++ raytracing/src/main/java/raytracing/Scene.java
@@ -21,20 +21,6 @@ public class Scene {
         this.lighting = lighting;
         this.bgColor = bgColor;
     }
-    /*
-    Intended example usage:
-    Body body0 = new SphereBody(Color.RED, 1.0f);
-    Body body1 = new SphereBody(Color.YELLOW, 2.0f);
-    Body[] bodies = {body0, body1};
-    Light light0 = new PointLight(new Tuple3f(10, 10, 10), Color.WHITE);
-    Light light1 = new PointLight(new Tuple3f(-10, 10, 10), Color.MAGENTA);
-    Light[] lights = {light0, light1};
-    RayCaster rayCaster = new NewtonRayCaster(1.0f / 32, 32, 16);
-    LightingModel lighting; // TODO: Instantiate.
-    Color bgColor = Color.BLACK;
-    Scene scene = new Scene(bodies, lights, rayCaster, lighting, bgColor);
-    Color colAtRay = scene.getRGB(ray); // Result! Call once for each pixel.
-    */
     
     /**
      * Returns color of first intersection of a given ray.
@@ -51,3 +37,17 @@ public class Scene {
         return color;
     }
 }
+/*
+Intended example usage:
+Body body0 = new SphereBody(Color.RED, 1.0f);
+Body body1 = new SphereBody(Color.YELLOW, 2.0f);
+Body[] bodies = {body0, body1};
+Light light0 = new PointLight(new Tuple3f(10, 10, 10), Color.WHITE);
+Light light1 = new PointLight(new Tuple3f(-10, 10, 10), Color.MAGENTA);
+Light[] lights = {light0, light1};
+RayCaster rayCaster = new NewtonRayCaster(1.0f / 32, 32, 16);
+LightingModel lighting; // TODO: Instantiate.
+Color bgColor = Color.BLACK;
+Scene scene = new Scene(bodies, lights, rayCaster, lighting, bgColor);
+Color colAtRay = scene.getRGB(ray); // Result! Call once for each pixel.
+*/
