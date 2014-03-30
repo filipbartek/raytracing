a7d2e43cff733a0c4180a09c971d8b5254082331
Filip Bártek <filip.bartek@hotmail.com>
2014-03-30 16:18:17 +0200

    LightingModel.getRGB(): Added description

--- raytracing/src/main/java/raytracing/LightingModel.java
+++ raytracing/src/main/java/raytracing/LightingModel.java
@@ -7,5 +7,11 @@ import java.awt.Color;
  * @author Filip Bartek
  */
 public interface LightingModel {
+    /**
+     * Get color of the intersection point with the given lights
+     * @param intersection
+     * @param lights
+     * @return Color of the point of intersection
+     */
     public Color getRGB(Intersection intersection, Light[] lights);
 }
