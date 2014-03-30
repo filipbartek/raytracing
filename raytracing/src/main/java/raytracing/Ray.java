7e0c1775dc37ca252707e5293d699a071352f89e
Filip Bártek <filip.bartek@hotmail.com>
2014-03-30 20:21:15 +0200

    Ray: Remembers previous call
    Minor optimization: return previous result immediately if called with
    the same `t` parameter as last time.

--- raytracing/src/main/java/raytracing/Ray.java
+++ raytracing/src/main/java/raytracing/Ray.java
@@ -8,10 +8,14 @@ package raytracing;
 public class Ray {
     public Tuple3f startingPoint;
     public Tuple3f dir;
+    private float tPrev;
+    private Tuple3f pointPrev;
     
     public Ray(Tuple3f startingPoint, Tuple3f dir) {
         this.startingPoint = startingPoint;
         this.dir = dir;
+        this.tPrev = Float.NaN;
+        this.pointPrev = null;
     }
     /*
     Basic usage:
@@ -22,10 +26,18 @@ public class Ray {
 
     public Tuple3f rayPoint(float t) {
         assert !Float.isNaN(t);
+        if (t == tPrev) {
+            // Previous call had the same `t` parameter.
+            // Reuse the previous ray point.
+            return (Tuple3f) pointPrev.clone();
+        }
         Tuple3f rayShift = (Tuple3f) dir.clone();
         rayShift.scale(t);
         Tuple3f rayPoint = (Tuple3f) startingPoint.clone();
         rayPoint.add(rayShift);
+        // Save the parameter and the result for possible optimization.
+        tPrev = t;
+        pointPrev = (Tuple3f) rayPoint.clone();
         return rayPoint;
     }
 }
