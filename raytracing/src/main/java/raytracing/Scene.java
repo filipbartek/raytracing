af917c2021f1b7b666ee9fd56135f3e01da138af
Filip Bártek <filip.bartek@hotmail.com>
2014-03-30 16:57:27 +0200

    Scene: Added bgColor; clarified a comment

--- raytracing/src/main/java/raytracing/Scene.java
+++ raytracing/src/main/java/raytracing/Scene.java
@@ -11,22 +11,28 @@ public class Scene {
     private final Light[] lights;
     private final RayCaster rayCaster;
     private final LightingModel lighting;
+    private final Color bgColor; // Background color
     
-    public Scene(Body[] bodies, Light[] lights, RayCaster rayCaster, LightingModel lighting) {
+    public Scene(Body[] bodies, Light[] lights, RayCaster rayCaster,
+            LightingModel lighting, Color bgColor) {
         this.bodies = bodies;
         this.lights = lights;
         this.rayCaster = rayCaster;
         this.lighting = lighting;
+        this.bgColor = bgColor;
     }
     
     /**
-     * Returns color of a given ray.
+     * Returns color of first intersection of a given ray.
      * The ray corresponds to a pixel in screen.
      * @param ray
      * @return color of the intersection
      */
     public Color getRGB(Ray ray) {
         Intersection intersection = rayCaster.castRay(ray, bodies);
+        if (!intersection.hitApprox) {
+            return bgColor;
+        }
         Color color = lighting.getRGB(intersection, lights);
         return color;
     }
