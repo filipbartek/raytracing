256502cb5ca5d0ac7c5c758ba7f1df69f7a08816
Filip Bártek <filip.bartek@hotmail.com>
2014-04-01 11:04:03 +0200

    Elipsoid: Added comments

--- raytracing/src/main/java/raytracing/Elipsoid.java
+++ raytracing/src/main/java/raytracing/Elipsoid.java
@@ -2,6 +2,11 @@ package raytracing;
 
 import java.awt.Color;
 
+/**
+ * http://en.wikipedia.org/wiki/Ellipsoid
+ * @author Neva
+ * @author Filip Bartek
+ */
 public class Elipsoid extends ColoredBody implements Body {
 
 	private final float a;
@@ -19,12 +24,23 @@ public class Elipsoid extends ColoredBody implements Body {
 	public float getA(){return a*a;}
 	public float getB(){return b*b;}
 	
+    /**
+     * Returns the value of implicit descriptive function of the ellipsoid
+     * Value: x^2/a^2 + y^2/b^2 + z^2/c^2 - 1
+     * @param p {x, y, z}
+     * @return Value of f(p)
+     */
     @Override
     public float f(float[/*3*/] p) {
         Tuple3f point = new Tuple3f(p);
         return (point.x * point.x)/getA() + (point.y * point.y)/getB() + (point.z * point.z)/getC() - 1;
     }
 
+    /**
+     * Value: 2x / a^2
+     * @param p
+     * @return Partial derivative of f with respect to x
+     */
     @Override
     public float fx(float[/*3*/] p) {
         Tuple3f point = new Tuple3f(p);
