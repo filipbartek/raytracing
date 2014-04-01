2e7d3a1923e1f4dc122f688f70a34c8ce975f4a5
Filip Bártek <filip.bartek@hotmail.com>
2014-04-01 17:19:59 +0200

    PrikazovalnikTest: Added test for rendering (profiling)
    Added a test case that renders a small scene. Can be used for
    incremental optimization using profiler.

---
+++ raytracing/src/test/java/PrikazovalnikTest.java
@@ -0,0 +1,71 @@
+import java.awt.Color;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import raytracing.Body;
+import raytracing.ColorInterpolation;
+import raytracing.Light;
+import raytracing.LightingModel;
+import raytracing.NewtonRayCaster;
+import raytracing.PointLight;
+import raytracing.RayCaster;
+import raytracing.Scene;
+import raytracing.SphereBody;
+import raytracing.TranslatedBody;
+import raytracing.Tuple3f;
+
+/**
+ *
+ * @author Filip Bartek
+ */
+public class PrikazovalnikTest {
+    
+    private Camera cam;
+    private Scene scene;
+    private int resX;
+    private int resY;
+    
+    private static Scene buildScene() {
+        Color colorLeft = Color.RED;
+        Color colorRight = Color.YELLOW;
+        Body body0 = new TranslatedBody(new SphereBody(colorRight, 0.75f), new Tuple3f(-1, 0, 0));
+        Body body1 = new TranslatedBody(new SphereBody(colorRight, 0.75f), new Tuple3f(1, 0, 0));
+        Body body2 = new TranslatedBody(new SphereBody(colorLeft, 1.0f), new Tuple3f(0, -1, 0));
+        Light light0 = new PointLight(new Tuple3f(0, 5, 0), Color.WHITE);
+        Light light1 = new PointLight(new Tuple3f(0, 5, 5), Color.WHITE);
+        float step = 1.0f / 32;
+        float limit = 32;
+        int approxSteps = 16;
+        LightingModel lighting = new ColorInterpolation();
+        Color bgColor = Color.DARK_GRAY;
+        
+        Body[] bodies = {body0, body1, body2};
+        Light[] lights = {light0, light1};
+        RayCaster rayCaster = new NewtonRayCaster(step, limit, approxSteps);
+        
+        return new Scene(bodies, lights, rayCaster, lighting, bgColor);
+    }
+    
+    @Before
+    public void setUp() {
+        float[] loc={0,-0.6f,4};
+        float[] dir={0,0,-1};
+        
+        cam = new Camera(loc,dir);
+        scene = buildScene();
+        
+        resX = 64;
+        resY = 64;
+    }
+    
+    /**
+     * Test of render method, of class Prikazovalnik.
+     */
+    @Test
+    public void testRender() {
+        System.out.println("render");
+        
+        Prikazovalnik instance = new Prikazovalnik();
+        instance.render(cam, scene, resX, resY);
+    }
+}
