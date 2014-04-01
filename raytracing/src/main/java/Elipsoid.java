cf09ca8be1dbf2efad213067123757ab70a99a4c
Filip Bártek <filip.bartek@hotmail.com>
2014-04-01 10:36:34 +0200

    Elipsoid, TorusBody: Imported from Neva

---
+++ raytracing/src/main/java/Elipsoid.java
@@ -0,0 +1,55 @@
+
+
+public class Elipsoid extends ColoredBody implements Body {
+    
+	private final float location;
+	private final float a;
+	private final float b;
+	private final float c;
+	
+	int d=1;
+    
+    public Elipsoid(Color color, float location, float a, float b, float c ) {
+        super(color);
+		this.location=location;
+		this.a=a;
+		this.b=b;
+		this.c=c;
+    }
+
+	public float getC(){return c*c;}
+	public float getA(){return a*a;}
+	public float getB(){return b*b;}
+	
+	public Tuple3f getLocation(){
+		return location;
+	}
+	public void setLocation(Tuple3f newL){
+		location=newL;
+	}
+	
+    @Override
+    public float f(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return (point.x * point.x)/getA() + (point.y * point.y)/getB() + (point.z * point.z)/getC() - d;
+    }
+
+    @Override
+    public float fx(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return (2 * point.x)/getA();
+    }
+
+    @Override
+    public float fy(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return (2 * point.y)/getB();
+    }
+
+    @Override
+    public float fz(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return (2 * point.z)*getC();
+    }
+    
+}
---
+++ raytracing/src/main/java/TorusBody.java
@@ -0,0 +1,47 @@
+public class TorusBody extends ColoredBody implements Body {
+    
+    private final float minorRadius;
+	private final float majorRadius;
+	private final float location;
+    
+    public TorusBody(Color color, float location, float minorRadius, float majorRadius) {
+        super(color);
+        this.minorRadius = minorRadius;
+		this.majorRadius = majorRadius;
+		this.location=location;
+    }
+
+	public gmajorRadius(){return majorRadius;}
+	public gminorRadius(){return minorRadius*minorRadius;}
+	
+	public Tuple3f getLocation(){
+		return location;
+	}
+	public void setLocation(Tuple3f newL){
+		location=newL;
+	}
+    @Override
+    public float f(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return (gmajorRadius()-Math.sqrt(point.x * point.x + point.y * point.y))*(gmajorRadius()-Math.sqrt(point.x * point.x + point.y * point.y)) + point.z * point.z - gminorRadius();
+    }
+
+    @Override
+    public float fx(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return 2 * point.x;
+    }
+
+    @Override
+    public float fy(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return 2 * point.y;
+    }
+
+    @Override
+    public float fz(float[/*3*/] p) {
+        Tuple3f point = new Tuple3f(p);
+        return 2 * point.z;
+    }
+    
+}
